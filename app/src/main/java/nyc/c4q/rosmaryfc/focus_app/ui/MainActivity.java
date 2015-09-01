31f0c6b22b2641b0fe1c1b82b4c252748ed7752e
hyunj0 <hyunj0@hotmail.com>
2015-09-01 15:17:17 -0400

    modify app monitor ui

--- app/src/main/java/nyc/c4q/rosmaryfc/focus_app/ui/MainActivity.java
+++ app/src/main/java/nyc/c4q/rosmaryfc/focus_app/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package nyc.c4q.rosmaryfc.focus_app.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,42 +9,32 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
-import nyc.c4q.rosmaryfc.focus_app.App;
+
+import nyc.c4q.rosmaryfc.focus_app.BlockService;
 import nyc.c4q.rosmaryfc.focus_app.BlockSessionFragment;
-import nyc.c4q.rosmaryfc.focus_app.DatabaseHelper;
+import nyc.c4q.rosmaryfc.focus_app.MonitoringAppsFragment;
 import nyc.c4q.rosmaryfc.focus_app.R;
-import nyc.c4q.rosmaryfc.focus_app.TestFragmentList;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private DrawerLayout mDrawerLayout;
-
-    DatabaseHelper databaseHelper;
-
-    List<App> monitoringApps;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        databaseHelper = DatabaseHelper.getInstance(this);
+        Intent intent = new Intent(this, BlockService.class);
+        this.startService(intent);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         final ActionBar actionBar = getSupportActionBar();
-
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
@@ -69,32 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        if (databaseHelper != null) {
-//            try {
-//                Dao<App, ?> appDao = databaseHelper.getDao(App.class);
-//                monitoringApps = appDao.query(appDao.queryBuilder().where().eq("APP_MONITOR", true).prepare());
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        if (monitoringApps.size() != 0) {
-//            MonitoringAppAdapter adapter = new MonitoringAppAdapter(this, monitoringApps);
-//            monitoring_app_list.setAdapter(adapter);
-//        }
-//    }
-
-
     private void setupViewPager(ViewPager viewPager) {
         Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new TestFragmentList(), "App Monitor ");
+        adapter.addFragment(new MonitoringAppsFragment(), "App Monitor ");
         adapter.addFragment(new BlockSessionFragment(), "Block Sessions");
 //        adapter.addFragment(new BlockTimes(), "BlockTimes");
-//        adapter.addFragment(new TestFragmentList(), "TBD");
+//        adapter.addFragment(new MonitoringAppsFragment(), "TBD");
         viewPager.setAdapter(adapter);
 
 
